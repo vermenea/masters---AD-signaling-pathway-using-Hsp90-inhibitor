f8599be22c785b90dc567ce6e6d3b7d999dd6ee9
vermenea <vermenea@gmail.com>
2024-11-28 16:54:09 +0100

    demo plot of stat1 phosphorylation

--- scripts/p-stat-1/analysis.py
+++ scripts/p-stat-1/analysis.py
@@ -1,8 +1,34 @@
+#imports
 import sys
 sys.path.append('/Users/vermenea/Documents/biologia/masters/analysis/scripts')
 
 from beta_actin_data import beta_actin_data
 from pstat1_data import data as pstat1_data
+import matplotlib.pyplot as plt
 
-print(beta_actin_data)
-print(pstat1_data)
+#preparing data
+control_beta_actin = beta_actin_data["control"][0]
+stimulation_beta_actin = beta_actin_data["stimulation"][0]
+treatment_01_beta_actin = beta_actin_data["0.1MM_treatment"][0]
+treatment_1_beta_actin = beta_actin_data["1MM_treatment"][0]
+
+control_ps1 = pstat1_data["control"][0]
+stimulation_ps1 = pstat1_data["stimulation"][0]
+treatment_01_ps1 = pstat1_data["0.1MM_treatment"][0]
+treatment_1_ps1 = pstat1_data["1MM_treatment"][0]
+
+normalized_control = control_beta_actin / control_ps1 
+normalized_stimulation = stimulation_beta_actin / stimulation_ps1 
+normalized_treatment_01 = treatment_01_beta_actin / treatment_01_ps1
+normalized_treatment_1 = treatment_1_beta_actin / treatment_1_ps1
+
+#plotting
+labels = ['Control', 'Stimulation', '0.1MM Treatment', '1MM Treatment']
+normalized_values = [normalized_control, normalized_stimulation, normalized_treatment_01, normalized_treatment_1]
+
+plt.figure(figsize=(10, 6))
+plt.bar(labels, normalized_values, color=['#FF0099', '#CC0099', '#990066', '#660066'])
+plt.xlabel('Normalized pSTAT1 Levels with Beta-actin')
+plt.ylabel('Normalized pSTAT1')
+plt.title('Phosphorylation of STAT1')
+plt.show()
